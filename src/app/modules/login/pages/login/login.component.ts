89a1123a607f6e6469006965565513b53aa8509d
machetio7 <jose.crperez@gmail.com>
2022-09-17 19:56:12 -0600

    feat(login component): Template html and css

--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('formBox') formBox!: ElementRef;
+  @ViewChild('signUpBtn') signUpBtn!: ElementRef;
+  @ViewChild('signInBtn') signInBtn!: ElementRef;
+  @ViewChild('auth') auth!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer:Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  setClassForm(type:number){
+    if(type===1){
+      this.renderer.addClass(this.formBox.nativeElement, 'active');
+      this.renderer.addClass(this.auth.nativeElement, 'active');
+    }else{
+      this.renderer.removeClass(this.formBox.nativeElement, 'active');
+      this.renderer.removeClass(this.auth.nativeElement, 'active');
+    }
+  }
 }
